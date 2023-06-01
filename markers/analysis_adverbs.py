8e4440e0e5e19b5c683688cc22b36c136f73b693
ken-sevenseas <inakanookaki@icloud.com>
2023-06-01 21:30:01 +0900

    change: input from txt file -> input from variable

--- markers/analysis_adverbs.py
+++ markers/analysis_adverbs.py
@@ -5,12 +5,6 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
 
-def load_text(file):
-    with open(file, 'r') as f:
-        text = f.read()
-    return text
-
-
 def identify_markers(text):
     sentences = sent_tokenize(text)
 
@@ -30,34 +24,22 @@ def identify_markers(text):
     return fronted_adverbs, regular_adverbs
 
 
-def analyze_text(file):
-    text = load_text(file)
+def analyze_text(text):
     fronted_adverbs, regular_adverbs = identify_markers(text)
-
-    print(f"Fronted Adverbs of Manner: {fronted_adverbs}")
-    print(f"Regular Adverbs of Manner: {regular_adverbs}")
-
-    if regular_adverbs == 0:
-        print("No regular adverbs of manner found.")
-    else:
-        print(
-            f"Ratio of Fronted Adverbs to Regular Adverbs: {fronted_adverbs / regular_adverbs}")
+    return fronted_adverbs, regular_adverbs
 
 
-analyze_text('a.txt')
-
-"""
-input_txt******************************************************************************************************************************************************************************************************************************************************
-    He was an old man who fished alone in a skiff in the Gulf Stream and he had gone eighty-four days now without taking a fish.
+if __name__ == "__main__":
+    text = """He was an old man who fished alone in a skiff in the Gulf Stream and he had gone eighty-four days now without taking a fish.
 In the first forty days a boy had been with him.
 But after forty days without a fish the boy’s parents had told him that the old man was now definitely and finally salao, which is the worst form of unlucky, and the boy had gone at their orders in another boat which caught three good fish the first week.
 It made the boy sad to see the old man come in each day with his skiff empty and he always went down to help him carry either the coiled lines or the gaff and harpoon and the sail that was furled around the mast.
 The sail was patched with flour sacks and, furled, it looked like the flag of permanent defeat.
 The old man was thin and gaunt with deep wrinkles in the back of his neck.
-The brown blotches of the benevolent skin cancer the sun brings from its reflection on the tropic sea were on his cheeks.
-***************************************************************************************************************************************************************************************************************************************************************
-output>>>
-Fronted Adverbs of Manner: 1
-Regular Adverbs of Manner: 3
-Ratio of Fronted Adverbs to Regular Adverbs: 0.3333333333333333
-    """
+The brown blotches of the benevolent skin cancer the sun brings from its reflection on the tropic sea were on his cheeks."""
+
+    fronted_adverbs, regular_adverbs = analyze_text(text)
+    print(f"Fronted Adverbs: {fronted_adverbs}")
+    print(f"Regular Adverbs: {regular_adverbs}")
+    print(
+        f"Ratio of Fronted Adverbs to Regular Adverbs: {fronted_adverbs / regular_adverbs}")
